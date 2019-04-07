78e95fb3dbabb0c0f56b77cda7257ff49d805e79
madalinbenchia <33199272+madalinbenchia@users.noreply.github.com>
2019-04-08 00:56:15 +0300

    CartPage v2

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,108 +1,85 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-cart',
-  templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+selector: 'app-cart',
+templateUrl: './cart.component.html',
+styleUrls: ['./cart.component.css']
 })
+
 export class CartComponent implements OnInit {
-	
-	public items : any = [
-		{
-			"wine_name": "Purcari Rose",
-			"price" : "45",
-			"description": "Vin roze sec, 13% vol. alc., produs la Vinaria Purcari in 2013, Republica Moldova"
-		},
-		{
-			"wine_name": "Purcari Black",
-			"price" : "30",
-			"description": "Vin rosu sec, 13% vol. alc., produs la Vinaria Purcari in 2009, Republica Moldova"
-		},
-		{
-			"wine_name": "Purcari White",
-			"price" : "50",
-			"description": "Vin alb sec, 13% vol. alc., produs la Vinaria Purcari in 2015, Republica Moldova"
-		}
-	];
+
+  public items : any = [{"id": "1", "wine_name": "Purcarii Wine", "price":"6", "description":"Acesta este o descriere"},{"id": "1", "wine_name": "Purcarii Wine", "price":"6", "description":"Acesta este o descriere"}];
+
+
+
   constructor() { }
 
   ngOnInit() {
 
-  
-var taxRate = 0.05;
-var shippingRate = 0; 
-var fadeTime = 300;
-
-
-recalculateCart();
-$('.product-quantity input').change( function() {
-  updateQuantity(this);
-});
-
-$('.product-removal button').click( function() {
-  removeItem(this);
-});
-
-
-
-
-function recalculateCart()
-{
-  var subtotal = 0;
-  
-  /* Sum up row totals */
-  $('.product').each(function () {
-    subtotal += parseFloat($(this).children('.product-line-price').text());
-  });
-  
-  
-  var tax = subtotal * taxRate;
-  var shipping = (subtotal > 0 ? shippingRate : 0);
-  var total = subtotal + tax + shipping;
-  
-  /* Update totals display */
-  $('.totals-value').fadeOut(fadeTime, function() {
-    $('#cart-subtotal').html(subtotal.toFixed(2));
-    $('#cart-tax').html(tax.toFixed(2));
-    $('#cart-shipping').html(shipping.toFixed(2));
-    $('#cart-total').html(total.toFixed(2));
-    if(total == 0){
-      $('.checkout').fadeOut(fadeTime);
-    }else{
-      $('.checkout').fadeIn(fadeTime);
-    }
-    $('.totals-value').fadeIn(fadeTime);
-  });
-}
-
-
-
-function updateQuantity(quantityInput)
-{
-  /* Calculate line price */
-  var productRow = $(quantityInput).parent().parent();
-  var price = parseFloat(productRow.children('.product-price').text());
-  var quantity = $(quantityInput).val();
-  var linePrice = price * quantity;
-  
- 
-  productRow.children('.product-line-price').each(function () {
-    $(this).fadeOut(fadeTime, function() {
-      $(this).text(linePrice.toFixed(2));
-      recalculateCart();
-      $(this).fadeIn(fadeTime);
-    });
-  });  
-}
-
-
-function removeItem(removeButton)
-{
-  var productRow = $(removeButton).parent().parent();
-  productRow.slideUp(fadeTime, function() {
-    productRow.remove();
-    recalculateCart();
-  });
-}
   }
+
+    recalculateCart()
+      {
+        var subtotal = 0;
+
+        var taxRate = 0.05;
+        var shippingRate = 0;
+        var fadeTime = 300;
+        /* Sum up row totals */
+        $('.product').each(function () {
+        subtotal += parseFloat($(this).children('.product-line-price').text());
+        });
+
+        var tax = subtotal * taxRate;
+        var shipping = (subtotal > 0 ? shippingRate : 0);
+        var total = subtotal + tax + shipping;
+
+        /* Update totals display */
+        $('.totals-value').fadeOut(fadeTime, function() {
+        $('#cart-subtotal').html(subtotal.toFixed(2));
+        $('#cart-tax').html(tax.toFixed(2));
+        $('#cart-shipping').html(shipping.toFixed(2));
+        $('#cart-total').html(total.toFixed(2));
+        if(total == 0){
+        $('.checkout').fadeOut(fadeTime);
+        }else{
+        $('.checkout').fadeIn(fadeTime);
+        }
+        $('.totals-value').fadeIn(fadeTime);
+        });
+      }
+
+    removeItem(removeButton : number)
+      {
+
+          var productRow = $('#' + removeButton);
+          console.log(productRow);
+          productRow.slideUp(300, function() {
+          productRow.remove();
+          recalculateCart();
+
+          });
+      }
+
+      updateQuantity(quantityInput: number)
+        {
+          /* Calculate line price */
+          var productRow = $('#' + quantityInput);
+
+          var price = parseFloat(productRow.children('.product-price').text());
+
+          var quantity = $('#'+ quantityInput).val();
+          var linePrice = price * quantity;
+          console.log(linePrice);
+
+
+          productRow.children('.product-line-price').each(function () {
+            $(this).fadeOut(300, function() {
+            $(this).text(linePrice.toFixed(2));
+            recalculateCart();
+            $(this).fadeIn(300);
+            });
+          });
+        }
+
 }
